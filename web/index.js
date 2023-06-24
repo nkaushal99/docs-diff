6f7b04370501424345529a0ec034160c9e61dd2d
Nikhil Kaushal <kaushalnikhil99@gmail.com>
2023-06-24 16:06:16 +0530

    feat: Add UI to upload the docs and display corresponding diffs (#13)

---
+++ web/index.js
@@ -0,0 +1,47 @@
+const api = 'http://localhost:8080/v1/compare';
+
+window.onload = function () {
+  const uploadForm = document.querySelector('#uploadForm');
+  uploadForm.onsubmit = function (event) {
+    event.preventDefault();
+    const file1 = document.querySelector('#oldFile').files[0];
+    const file2 = document.querySelector('#newFile').files[0];
+
+    if (file1 && file2) {
+      const reader1 = new FileReader();
+      reader1.readAsDataURL(file1);
+
+      const reader2 = new FileReader();
+      reader2.readAsDataURL(file2);
+
+      // Create a new FormData object
+      const formData = new FormData();
+
+      // Append the files to the FormData object
+      formData.append('oldFile', file1);
+      formData.append('newFile', file2);
+
+      // Make a POST request to the API endpoint
+      fetch(api, {
+        method: 'POST',
+        body: formData
+      })
+        .then(response => response.json())
+        .then(data => {
+          // Handle the response data
+          const oldFileIframe = document.querySelector('#oldFileHltd');
+          oldFileIframe.src = oldFileIframe.src + data.oldFileBytes;
+          oldFileIframe.hidden = false;
+
+          const newFileIframe = document.querySelector('#newFileHltd');
+          newFileIframe.src = newFileIframe.src + data.newFileBytes;
+          newFileIframe.hidden = false;
+        })
+        .catch(error => {
+          // Handle any errors that occur during the request
+          console.error(error);
+        });
+    }
+  };
+};
+
