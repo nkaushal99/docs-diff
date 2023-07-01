3e110be25477101ab36a2ea8a26bd2798cb813f7
Nikhil Kaushal <kaushalnikhil99@gmail.com>
2023-07-02 02:36:25 +0530

    UI beautify (#14)

--- web/index.js
+++ web/index.js
@@ -1,47 +1,148 @@
-const api = 'http://localhost:8080/v1/compare';
+// global variables to store files uploaded by the user
+let oldFile, newFile;
 
-window.onload = function () {
-  const uploadForm = document.querySelector('#uploadForm');
-  uploadForm.onsubmit = function (event) {
-    event.preventDefault();
-    const file1 = document.querySelector('#oldFile').files[0];
-    const file2 = document.querySelector('#newFile').files[0];
+// handle file upload events using 'browse' dialog
+const oldFileBrowser = document.querySelector('#old-file-browser');
+const oldFileUpload = document.querySelector('#old-file-upload');
+oldFileBrowser.addEventListener('click', event => {
+  event.preventDefault();
+  oldFileUpload.click();
+})
 
-    if (file1 && file2) {
-      const reader1 = new FileReader();
-      reader1.readAsDataURL(file1);
+const newFileBrowser = document.querySelector('#new-file-browser');
+const newFileUpload = document.querySelector('#new-file-upload');
+newFileBrowser.addEventListener('click', event => {
+  event.preventDefault();
+  newFileUpload.click();
+});
 
-      const reader2 = new FileReader();
-      reader2.readAsDataURL(file2);
-
-      // Create a new FormData object
-      const formData = new FormData();
-
-      // Append the files to the FormData object
-      formData.append('oldFile', file1);
-      formData.append('newFile', file2);
-
-      // Make a POST request to the API endpoint
-      fetch(api, {
-        method: 'POST',
-        body: formData
-      })
-        .then(response => response.json())
-        .then(data => {
-          // Handle the response data
-          const oldFileIframe = document.querySelector('#oldFileHltd');
-          oldFileIframe.src = oldFileIframe.src + data.oldFileBytes;
-          oldFileIframe.hidden = false;
-
-          const newFileIframe = document.querySelector('#newFileHltd');
-          newFileIframe.src = newFileIframe.src + data.newFileBytes;
-          newFileIframe.hidden = false;
-        })
-        .catch(error => {
-          // Handle any errors that occur during the request
-          console.error(error);
-        });
+// Create a function that returns the callback function with the variable to change as the argument
+function updateFileVar(fileVar) {
+  return function (event) {
+    // let the default event handling happen i.e. dont override preventDefault()
+    // update the file variable
+    if (fileVar === 'oldFile') {
+      oldFile = event.target.files[0];
+    } else {
+      newFile = event.target.files[0];
     }
   };
+}
+
+// update oldFile / newFile values when files uploaded
+oldFileUpload.addEventListener('change', updateFileVar('oldFile'));
+newFileUpload.addEventListener('change', updateFileVar('newFile'));
+
+// const borderContainers = document.querySelectorAll('.border-container');
+function handleDragDropInAnimation(event) {
+  if (event.currentTarget.classList.contains('border-container')) {
+    // Set the cursor to indicate a copy operation
+    event.dataTransfer.dropEffect = "copy";
+    event.currentTarget.style.borderStyle = 'solid';
+  }
+}
+
+function handleDragDropOutAnimation(event) {
+  if (event.currentTarget.classList.contains('border-container'))
+    event.currentTarget.style.borderStyle = 'dashed';
+}
+
+// Handle the dragover and dragenter event
+function handleDragOver(event) {
+  event.preventDefault();
+  handleDragDropInAnimation(event);
+}
+
+function handleDragEnter(event) {
+  handleDragOver(event);
+}
+
+// Handle the dragleave event
+function handleDragLeave(event) {
+  event.preventDefault();
+  // Set the cursor to indicate a copy operation
+  handleDragDropOutAnimation(event);
+}
+
+// handle the drop event
+function updateFileVarWithDrop(fileVar) {
+  return function dropHandler(event) {
+    console.log('file dropped')
+    // Prevent default behavior (Prevent file from being opened)
+    event.preventDefault();
+
+    handleDragLeave(event);
+    const files = event.dataTransfer.files;
+    if (files) {
+      const file = files[0];
+      // update the file variable
+      if (fileVar === 'old') {
+        oldFile = file;
+      } else {
+        newFile = file;
+      }
+    }
+  };
+}
+
+// disable window drag n drop listener
+window.addEventListener('drop', event => {
+  event.preventDefault();
+});
+window.addEventListener('dragover', event => {
+  event.preventDefault();
+})
+
+const dropZones = document.querySelectorAll('.drop-zone');
+dropZones.forEach(dropZone => {
+  dropZone.addEventListener('dragenter', handleDragEnter);
+  dropZone.addEventListener('dragover', handleDragOver);
+  if (dropZone.classList.contains('old')) {
+    dropZone.addEventListener('drop', updateFileVarWithDrop('old'));
+  } else {
+    dropZone.addEventListener('drop', updateFileVarWithDrop('new'));
+  }
+  dropZone.addEventListener('dragleave', handleDragLeave);
+});
+
+const uploadForm = document.querySelector('#upload-form');
+uploadForm.onsubmit = function (event) {
+  event.preventDefault();
+  const file1 = oldFile;
+  const file2 = newFile;
+
+  if (file1 && file2) {
+    const reader1 = new FileReader();
+    reader1.readAsDataURL(file1);
+
+    const reader2 = new FileReader();
+    reader2.readAsDataURL(file2);
+
+    // Create a new FormData object
+    const formData = new FormData();
+
+    // Append the files to the FormData object
+    formData.append('oldFile', file1);
+    formData.append('newFile', file2);
+
+    // Make a POST request to the API endpoint
+    const api = 'http://localhost:8080/v1/compare';
+    fetch(api, {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Store the response in session storage
+        sessionStorage.setItem('compareResponse', JSON.stringify(data));
+
+        // Redirect to diff.html
+        window.location.href = 'diff.html';
+      })
+      .catch(error => {
+        // Handle any errors that occur during the request
+        console.error(error);
+      });
+  }
 };
 
